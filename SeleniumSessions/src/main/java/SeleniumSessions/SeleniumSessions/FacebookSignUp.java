526c215da4c1e6004bdfd8d512fefb8ff95872fd
JASMEET KAUR <JASMEET KAUR@JASMEETKAUR-PC>
2020-08-08 20:02:32 +0530

    Pushing Facebook SignUp Code

--- SeleniumSessions/src/main/java/SeleniumSessions/SeleniumSessions/FacebookSignUp.java
+++ SeleniumSessions/src/main/java/SeleniumSessions/SeleniumSessions/FacebookSignUp.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FacebookSignUp {
+public class FacebookSignUp	 {
 	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","../SeleniumSessions/chromedriver.exe");
@@ -14,7 +14,7 @@ public class FacebookSignUp {
 	
 		WebElement firstName = driver.findElement(By.cssSelector("input#u_0_m"));
 		firstName.sendKeys("Jasmeet");
-		WebElement lastName = driver.findElement(By.name("lastname"));
+		WebElement lastName = driver.findElement(By.cssSelector("input[id='u_0_o']"));
 		lastName.sendKeys("Chadha");
 		WebElement email = driver.findElement(By.cssSelector("input[id='u_0_r']"));
 		email.sendKeys("7973845478");
